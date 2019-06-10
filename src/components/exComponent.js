fb296fe2b2efc80e574bfa91cd4fc6eb5d4aaa0e
GoVeggan <12maja1935@gmail.com>
2019-06-10 21:50:38 +0200

    pulled from beeinger

--- src/components/exComponent.js
+++ src/components/exComponent.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { Subscribe } from "unstated";
 
 export default class exComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div>aaa</div>
-    )
-  }
+    render() {
+        return ( <
+            div > bbb < /div>
+        )
+    }
 }
