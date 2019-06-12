a72d326d2db53a8c94da9d2f7acb396e59671cd9
GoVeggan <12maja1935@gmail.com>
2019-06-12 17:55:45 +0200

    pieski

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
-import './styles/App.css';
+import "./styles/App.css";
 import { Switch, Route } from "react-router-dom";
 import View from "./views/View";
-import Login from "./views/Login"
-import { Navbar, Nav, FormControl, Button,Form } from "react-bootstrap";
+import Login from "./views/Login";
+import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
     return (
-      <div>
+      <div
+        style={{
+          background: "rgb(242, 234, 201, 1)",
+          height: "100%",
+          minHeight: "100vh"
+        }}
+      >
         <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="/"><img style={{height: "10%", width: "10%"}} src={ require('./resources/dog.png') } /></Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+              style={{ height: "10%", width: "10%" }}
+              src={require("./resources/dog.png")}
+            />
+          </Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
@@ -26,16 +36,10 @@ export default class App extends Component {
           </Form>
         </Navbar>
         <Switch>
-          <Route
-            exact
-            path="/"
-            component={View}
-          />
+          <Route exact path="/" component={View} />
           <Route path="/login" component={Login} />
         </Switch>
       </div>
     );
   }
-
 }
-
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import { Subscribe } from "unstated";
-import { Button } from "react-bootstrap";
+import { Button, Form, Container, Col } from "react-bootstrap";
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
   }
 
+  a() {
+    console.log("kocham maciusia");
+  }
+
   render() {
+    console.log("Kocham Gosie");
     return (
-      <div>
-         <Button variant="warning" href="/">Main page</Button>
-      </div>
-    )
+      <Container style={{ width: "25vw", marginTop: "10vh" }}>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <div
+            className="d-flex flex-column"
+            style={{
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <Button variant="primary" onClick={this.a}>
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Container>
+    );
   }
 }
